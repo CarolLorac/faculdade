427b12f1bb5cdc95b2207a95bb04a8e370812d5c
Caroline Souza <caroline.souzab12@gmail.com>
2022-09-04 18:39:49 -0300

    Melhorando switch...case e adicionando do...while

--- string.c
+++ string.c
@@ -1,53 +1,110 @@
-//Fazer um programa leia uma string que represente um número de zero a dez e converta para o respectivo número e devolva
+//Fazer um programa leia uma string que represente um nÃºmero de zero a dez e converta para o respectivo nÃºmero e devolva
 //a quantidade de caracteres. Por xemplo: DEZ 10  3 caracteres
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> //permite usar strlen 
 
 void main()
 {
 	char numero[6];
-	int qtdCaracteres, valorNumero;
+	int qtdCaracteres, valorNumero, reiniciar = 1;
 	
-	printf("Digite o numero (em extenso) que deseja converter: ");
-	scanf("%s", numero);
-	
-	qtdCaracteres = strlen(numero);
-	
-	switch (qtdCaracteres)
+	do
 	{
-		case 2:
-			valorNumero = 1;
-			break;
-		case 3:
-			valorNumero = 10;
-			break;
-		case 4:
-			if (numero[0] == 's')
-			{
-				if (numero[qtdCaracteres - 1] == 's')
-					valorNumero = 6;
-				else
-					valorNumero = 7;
-			}
-			else if (numero[0] == 'd')
-				valorNumero = 2;
-			else if (numero[0] == 't')
-				valorNumero = 3;
-			else if (numero[0] == 'o')
-				valorNumero = 8;
-			else
-				valorNumero = 9;
-				break;
-		case 5:
-			if (numero[0] == 'c')
-				valorNumero = 5;
-			else
-				valorNumero = 4;
-			break;
-	}
-	
-	printf("\nNúmero (extenso): %s", numero);
-	printf("\nNúmero (inteiro): %i", valorNumero);
-	printf("\nQuantidade caracteres: %i", qtdCaracteres);
+	    printf("\nDigite o numero (em extenso) que deseja converter: ");
+        scanf("%s", numero);
+        
+        /*for (int i = 0; numero[i] != "\0"; i++)
+        {
+            numero[i] = tolower(numero[i]);
+        }*/
+        
+        qtdCaracteres = strlen(numero);
+        
+        switch (qtdCaracteres)
+        {
+        	case 2:
+        	    if (strcmp("um", numero) == 0) // 0 indica que os valores sÃ£o iguais
+        	    {
+        	        valorNumero = 1;
+        		    break;
+        	    }
+        	    
+        	case 3:
+        		if (strcmp("dez", numero) == 0)
+        		{
+        		    valorNumero = 10;
+        		    break;
+        		}
+        		
+        	case 4:
+        		if (numero[0] == 's')
+        		{
+        			if (strcmp("seis", numero) == 0)
+        			{
+        			    valorNumero = 6;
+        				break;
+        			}
+        			if (strcmp("sete", numero) == 0)
+        			{
+        			    valorNumero = 7;
+        				break;
+        			}
+        		}
+        		
+        		if (strcmp("dois", numero) == 0)
+        		{
+        		    valorNumero = 2;
+        		    break;
+        		}
+        			
+        		if (strcmp("tres", numero) == 0)
+        		{
+        		    valorNumero = 3;
+        		    break;
+        		}
+        			
+        		if (strcmp("oito", numero) == 0)
+        		{
+        		    valorNumero = 8;
+        		    break;
+        		}
+        			
+        		if (strcmp("nove", numero) == 0)
+        		{
+        		    valorNumero = 9;
+        			break;
+        		}
+        		
+        	case 5:
+        	    if (strcmp("cinco", numero) == 0)
+        	    {
+        	        valorNumero = 5;
+        		    break;
+        	    }
+        	    
+        	case 6:
+        	    if (strcmp("quatro", numero) == 0)
+        	    {
+        	        valorNumero = 4;
+        	        break;
+        	    }
+        	    
+        	default:
+        	    printf("Esse valor nÃ£o existe no intervalo determinado.");
+        }
+        
+        printf("\nNÃºmero (extenso): %s", numero);
+        if (valorNumero >= 0 && valorNumero <= 10)
+        {
+            printf("\nNÃºmero (inteiro): %i", valorNumero);
+        }
+        printf("\nQuantidade caracteres: %i", qtdCaracteres);   
+        
+        
+        printf("\nDeseja reiniciar? (0 - NÃƒO | 1 - SIM)");
+        scanf("%i", &reiniciar);
+        
+	} while (reiniciar == 1);
 }
