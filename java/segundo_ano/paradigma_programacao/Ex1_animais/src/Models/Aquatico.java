2857aff39aa6e291886f0bd4b44964def97ac97d
Caroline Souza <caroline.souza@tribofox.com.br>
2023-04-28 07:32:31 -0300

    Adicionando exercícios ASD

--- java/segundo_ano/paradigma_programacao/Ex1_animais/src/Models/Aquatico.java
+++
@@ -1,7 +0,0 @@
-package Models;
-
-public class Aquatico extends Animais
-{
-    public static String caraAquatico1= "\nVivem na maior parte do tempo dentro da agua, seja ela de rios, lagos ou mares";
-    public static String caraAquatico2= "\nPodem ser animais (vertebrados e invertebrados)" + "ou outras formas de vida como, por exemplo, plantas, algas e planctons.\n";
-}
